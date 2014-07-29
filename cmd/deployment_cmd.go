876a6ed7571ee455e0339c14adc54cbf3488daa9
Michael Maximilien <maxim@us.ibm.com>
2014-07-29 14:39:00 -0700

    Completed DeploymentCmd implementation
    [#73562448]
    
    Signed-off-by: Phan Le <ple@pivotallabs.com>

--- cmd/deployment_cmd.go
+++ cmd/deployment_cmd.go
@@ -2,6 +2,15 @@ package cmd
 
 import (
 	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/user"
+	"path"
+)
+
+const (
+	BOSH_MICRO_FILENAME = ".bosh_micro"
 )
 
 type deploymentCmd struct {
@@ -15,5 +24,29 @@ func NewDeploymentCmd() *deploymentCmd {
 func (f *deploymentCmd) Run(args []string) error {
 	f.args = args
 
-	return errors.New("Implement me!")
+	if f.args == nil {
+		return errors.New("Deployment command argument cannot be nil")
+	}
+
+	if len(f.args) < 1 {
+		return errors.New("Deployment command arguments must have at least one arg")
+	}
+
+	manifestFilePath := f.args[0]
+	if _, err := os.Stat(manifestFilePath); os.IsNotExist(err) {
+		return errors.New(fmt.Sprintf("Deployment command manifest path %s does not exist", manifestFilePath))
+	}
+
+	usr, err := user.Current()
+	if err != nil {
+		return errors.New("Could not access current user")
+	}
+
+	boshMicroPath := path.Join(usr.HomeDir, BOSH_MICRO_FILENAME)
+	err = ioutil.WriteFile(boshMicroPath, []byte(manifestFilePath), os.ModePerm)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Could not write to file %s", boshMicroPath))
+	}
+
+	return nil
 }
--- cmd/deployment_cmd_test.go
+++ cmd/deployment_cmd_test.go
@@ -1,5 +1,73 @@
 package cmd_test
 
-// Create new DeploymentCmd
-// verify validate args (valid and invalid)
-// store manifestPath to a file (.bosh-micro-cli)
+import (
+	"io/ioutil"
+	"os"
+	"os/user"
+	"path"
+
+	cmd "github.com/cloudfoundry/bosh-micro-cli/cmd"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("DeploymentCmd", func() {
+	var command cmd.Cmd
+	var manifestPath string
+	var args []string
+
+	BeforeEach(func() {
+		command = cmd.NewDeploymentCmd()
+		Expect(command).ToNot(BeNil())
+	})
+
+	Context("#Run", func() {
+		Context("ran with valid args", func() {
+			BeforeEach(func() {
+				file, err := ioutil.TempFile("", "bosh-micro-cli-manifest")
+				Expect(err).ToNot(HaveOccurred())
+
+				manifestPath = file.Name()
+				args = []string{manifestPath}
+			})
+
+			AfterEach(func() {
+				err := os.RemoveAll(manifestPath)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("stores the manifest file path to a hidden file at the home dir", func() {
+				err := command.Run(args)
+				Expect(err).ToNot(HaveOccurred())
+				usr, err := user.Current()
+				Expect(err).ToNot(HaveOccurred())
+
+				expectedFilePath := path.Join(usr.HomeDir, ".bosh_micro")
+				expectedFileContent, err := ioutil.ReadFile(expectedFilePath)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(string(expectedFileContent)).To(ContainSubstring(manifestPath))
+			})
+		})
+
+		Context("ran with invalid args", func() {
+			It("fails when manifest file path is nil", func() {
+				err := command.Run(nil)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Deployment command argument cannot be nil"))
+			})
+
+			It("fails when manifest file path is empty", func() {
+				err := command.Run([]string{})
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Deployment command arguments must have at least one arg"))
+			})
+
+			It("fails when manifest file path does not exist", func() {
+				err := command.Run([]string{"fake/manifest/path"})
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("Deployment command manifest path fake/manifest/path does not exist"))
+			})
+		})
+	})
+})
