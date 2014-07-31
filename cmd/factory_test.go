b189345171a717e0ed43e7c375a223f5b3614f6c
Andrea Law <alaw@pivotallabs.com>
2014-07-31 11:47:41 -0700

    Refactor cmd.Factory tests
    Signed-off-by: Adam Stegman <astegman@pivotallabs.com>

--- cmd/factory_test.go
+++ cmd/factory_test.go
@@ -1,36 +1,44 @@
 package cmd_test
 
 import (
+	"os"
+	"os/user"
+
 	boshlog "github.com/cloudfoundry/bosh-agent/logger"
 	boshsys "github.com/cloudfoundry/bosh-agent/system"
-	cmd "github.com/cloudfoundry/bosh-micro-cli/cmd"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	. "github.com/cloudfoundry/bosh-micro-cli/cmd"
+	bmui "github.com/cloudfoundry/bosh-micro-cli/ui"
 )
 
 var _ = Describe("cmd.Factory", func() {
-	var factory cmd.Factory
-	var logger boshlog.Logger
-	var filesystem boshsys.FileSystem
+	var (
+		factory    Factory
+		logger     boshlog.Logger
+		filesystem boshsys.FileSystem
+		ui         bmui.UI
+		usr        *user.User
+	)
 
 	BeforeEach(func() {
 		logger = boshlog.NewLogger(boshlog.LevelDebug)
-		factory = cmd.NewFactory(logger)
+		factory = NewFactory(logger)
 		filesystem = boshsys.NewOsFileSystem(logger)
+		ui = bmui.NewDefaultUI(os.Stdout, os.Stderr)
+		usr, _ = user.Current()
 	})
 
 	It("creates a new factory", func() {
 		Expect(factory).ToNot(BeNil())
-		Expect(filesystem).ToNot(BeNil())
 	})
 
 	Context("passing correct command name", func() {
 		It("has deployment command", func() {
 			cmd, err := factory.CreateCommand("deployment")
 			Expect(err).ToNot(HaveOccurred())
-			Expect(cmd).ToNot(BeNil())
-			Expect(cmd.FileSystem()).ToNot(BeNil())
+			Expect(cmd).To(Equal(NewDeploymentCmd(ui, usr.HomeDir, filesystem)))
 		})
 	})
 
