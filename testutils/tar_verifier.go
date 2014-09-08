d72bcdfb70f544bacf1ace5c38c55280237db600
Maria Shaldibina <mariash@pivotallabs.com>
2014-09-08 15:45:59 -0700

    Move BlobReader to testutils.TarVerifier
    Signed-off-by: Phan Le <ple@pivotallabs.com>

---
+++ testutils/tar_verifier.go
@@ -0,0 +1,26 @@
+package testutils
+
+import (
+	"fmt"
+)
+
+type TarVerifier struct {
+	BlobPath string
+}
+
+func (b TarVerifier) FileExists(fileName string) (bool, error) {
+	session, err := RunCommand("tar", "-tf", b.BlobPath, fmt.Sprintf("./%s", fileName))
+	if err != nil {
+		return false, err
+	}
+	return session.ExitCode() == 0, nil
+}
+
+func (b TarVerifier) FileContents(fileName string) ([]byte, error) {
+	session, err := RunCommand("tar", "--to-stdout", "-xf", b.BlobPath, fmt.Sprintf("./%s", fileName))
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return session.Out.Contents(), nil
+}
