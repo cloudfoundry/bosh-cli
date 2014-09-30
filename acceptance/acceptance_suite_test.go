4a7b2d0c91c4a2c0d391311c49c0c867666f77d0
Kris Hicks <krishicks@gmail.com>
2014-09-30 12:50:40 -0700

    Update acceptance_suite_test
    RegisterFailHandler takes Fail rather than FailHandler
    
    Signed-off-by: Phan Le <ple@pivotal.io>

--- acceptance/acceptance_suite_test.go
+++ acceptance/acceptance_suite_test.go
@@ -7,16 +7,7 @@ import (
 	"testing"
 )
 
-var (
-	suiteFailed bool
-)
-
-func FailHandler(message string, callerSkip ...int) {
-	suiteFailed = true
-	Fail(message, callerSkip...)
-}
-
 func TestAcceptance(t *testing.T) {
-	RegisterFailHandler(FailHandler)
+	RegisterFailHandler(Fail)
 	RunSpecs(t, "Acceptance Suite")
 }
