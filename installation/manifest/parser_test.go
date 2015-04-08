4eff518c4ba2a79aef71ac9ee56714b0f3b9d80e
Aaron Hurley <ahurley@pivotal.io>
2015-04-08 09:47:24 -0700

    Expand the tilde as $HOME for private key path.
    Cross compiling linux binaries from darwin will break as os/user is used
    to get the current user. Let's see how cross compilation works in Go 1.5
    before trying to fix this. [#88550996]
    
    Signed-off-by: caleb miles <cmiles@pivotal.io>

--- installation/manifest/parser_test.go
+++ installation/manifest/parser_test.go
@@ -2,6 +2,7 @@ package manifest_test
 
 import (
 	"errors"
+	"os/user"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -20,15 +21,114 @@ var _ = Describe("Parser", func() {
 		comboManifestPath string
 		fakeFs            *fakesys.FakeFileSystem
 		parser            Parser
+		logger            boshlog.Logger
 	)
 
 	BeforeEach(func() {
-		comboManifestPath = "fake-deployment-path"
 		fakeFs = fakesys.NewFakeFileSystem()
-		logger := boshlog.NewLogger(boshlog.LevelNone)
+		logger = boshlog.NewLogger(boshlog.LevelNone)
 		parser = NewParser(fakeFs, logger)
+		comboManifestPath = "fake-deployment-manifest"
+	})
 
-		contents := `
+	Context("when combo manifest path does not exist", func() {
+		It("returns an error", func() {
+			_, err := parser.Parse(comboManifestPath)
+			Expect(err).To(HaveOccurred())
+		})
+	})
+
+	Context("when parser fails to read the combo manifest file", func() {
+		JustBeforeEach(func() {
+			fakeFs.WriteFileString(comboManifestPath, "---\n")
+			fakeFs.ReadFileError = errors.New("fake-read-file-error")
+		})
+
+		It("returns an error", func() {
+			_, err := parser.Parse(comboManifestPath)
+			Expect(err).To(HaveOccurred())
+		})
+	})
+
+	Context("when the combo manifest file refrences ~", func() {
+		Context("when the ~ begins the path", func() {
+			It("expands ~/ into the current user's home directory", func() {
+				manifest := `
+---
+name: fake-deployment-name
+cloud_provider:
+  template:
+    name: fake-cpi-job-name
+    release: fake-cpi-release-name
+  ssh_tunnel:
+    host: 54.34.56.8
+    port: 22
+    user: fake-ssh-user
+    private_key: ~/fake-ssh-key.pem
+  agent_env_service: registry
+  mbus: http://fake-mbus-user:fake-mbus-password@0.0.0.0:6868
+  registry:
+    username: fake-registry-username
+    password: fake-registry-password
+    host: fake-registry-host
+    port: 123
+  properties:
+    fake-property-name:
+      nested-property: fake-property-value
+`
+				manifestPath := "4d89da25-454a-4c96-9f70-7bc9b90fee53"
+				fakeFs.WriteFileString(manifestPath, manifest)
+
+				currentUser, err := user.Current()
+				Expect(err).ToNot(HaveOccurred())
+
+				installationManifest, err := parser.Parse(manifestPath)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(installationManifest.SSHTunnel.PrivateKey).To(ContainSubstring(currentUser.HomeDir))
+			})
+		})
+
+		Context("when ~ occurs anywhere else in the path", func() {
+			It("assumes the user desired a literal ~", func() {
+				manifest := `
+---
+name: fake-deployment-name
+cloud_provider:
+  template:
+    name: fake-cpi-job-name
+    release: fake-cpi-release-name
+  ssh_tunnel:
+    host: 54.34.56.8
+    port: 22
+    user: fake-ssh-user
+    private_key: /some-root/~/fake-ssh-key.pem
+  agent_env_service: registry
+  mbus: http://fake-mbus-user:fake-mbus-password@0.0.0.0:6868
+  registry:
+    username: fake-registry-username
+    password: fake-registry-password
+    host: fake-registry-host
+    port: 123
+  properties:
+    fake-property-name:
+      nested-property: fake-property-value
+`
+				currentUser, err := user.Current()
+				Expect(err).ToNot(HaveOccurred())
+
+				manifestPath := "1a56d58a-0d56-4743-bbc5-5a9056af8bed"
+				fakeFs.WriteFileString(manifestPath, manifest)
+
+				installationManifest, err := parser.Parse(manifestPath)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(installationManifest.SSHTunnel.PrivateKey).ToNot(ContainSubstring(currentUser.HomeDir))
+			})
+		})
+	})
+
+	Context("with a valid manifest", func() {
+		JustBeforeEach(func() {
+			contents := `
 ---
 name: fake-deployment-name
 cloud_provider:
@@ -51,61 +151,39 @@ cloud_provider:
     fake-property-name:
       nested-property: fake-property-value
 `
-		fakeFs.WriteFileString(comboManifestPath, contents)
-	})
+			fakeFs.WriteFileString(comboManifestPath, contents)
+		})
 
-	Context("when combo manifest path does not exist", func() {
-		BeforeEach(func() {
-			err := fakeFs.RemoveAll(comboManifestPath)
+		It("parses installation from combo manifest", func() {
+			installationManifest, err := parser.Parse(comboManifestPath)
 			Expect(err).ToNot(HaveOccurred())
-		})
 
-		It("returns an error", func() {
-			_, err := parser.Parse(comboManifestPath)
-			Expect(err).To(HaveOccurred())
-		})
-	})
-
-	Context("when parser fails to read the combo manifest file", func() {
-		BeforeEach(func() {
-			fakeFs.ReadFileError = errors.New("fake-read-file-error")
-		})
-
-		It("returns an error", func() {
-			_, err := parser.Parse(comboManifestPath)
-			Expect(err).To(HaveOccurred())
-		})
-	})
-
-	It("parses installation from combo manifest", func() {
-		installationManifest, err := parser.Parse(comboManifestPath)
-		Expect(err).ToNot(HaveOccurred())
-
-		Expect(installationManifest).To(Equal(Manifest{
-			Name: "fake-deployment-name",
-			Template: ReleaseJobRef{
-				Name:    "fake-cpi-job-name",
-				Release: "fake-cpi-release-name",
-			},
-			Registry: Registry{
-				Username: "fake-registry-username",
-				Password: "fake-registry-password",
-				Host:     "fake-registry-host",
-				Port:     123,
-			},
-			AgentEnvService: "registry",
-			Properties: bmproperty.Map{
-				"fake-property-name": bmproperty.Map{
-					"nested-property": "fake-property-value",
+			Expect(installationManifest).To(Equal(Manifest{
+				Name: "fake-deployment-name",
+				Template: ReleaseJobRef{
+					Name:    "fake-cpi-job-name",
+					Release: "fake-cpi-release-name",
 				},
-			},
-			SSHTunnel: SSHTunnel{
-				Host:       "54.34.56.8",
-				Port:       22,
-				User:       "fake-ssh-user",
-				PrivateKey: "/tmp/fake-ssh-key.pem",
-			},
-			Mbus: "http://fake-mbus-user:fake-mbus-password@0.0.0.0:6868",
-		}))
+				Registry: Registry{
+					Username: "fake-registry-username",
+					Password: "fake-registry-password",
+					Host:     "fake-registry-host",
+					Port:     123,
+				},
+				AgentEnvService: "registry",
+				Properties: bmproperty.Map{
+					"fake-property-name": bmproperty.Map{
+						"nested-property": "fake-property-value",
+					},
+				},
+				SSHTunnel: SSHTunnel{
+					Host:       "54.34.56.8",
+					Port:       22,
+					User:       "fake-ssh-user",
+					PrivateKey: "/tmp/fake-ssh-key.pem",
+				},
+				Mbus: "http://fake-mbus-user:fake-mbus-password@0.0.0.0:6868",
+			}))
+		})
 	})
 })
