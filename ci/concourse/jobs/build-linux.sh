5872e05077ba339b289cc8125bdcb005077762b4
Tyler Schultz <tschultz@pivotal.io>
2015-04-17 16:46:20 -0700

    echo bosh init version
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- ci/concourse/jobs/build-linux.sh
+++ ci/concourse/jobs/build-linux.sh
@@ -5,13 +5,15 @@ export GOPATH=$(pwd)/gopath
 export GOARCH=amd64
 export GOOS=linux
 
-echo "go is at        -- $(which go)"
-echo "working dir is  -- $(pwd)"
-echo "working dir has -- $(ls -la)"
+echo "go is at          -- $(which go)"
+echo "working dir is    -- $(pwd)"
+echo "working dir has   -- $(ls -la)"
+echo "bosh init version -- ${BOSH_INIT_VERSION}"
 
 cd gopath/src/github.com/cloudfoundry/bosh-init
 # TODO: place the version file somewhere
 bin/build
+# mv out/bosh-init out/bosh-init-foo
 
 # GOARCH=amd64 GOOS=darwin
 # GOARCH=amd64 GOOS=windows
