dfe44d9c669d3b499c9c594aea0fb80d12d0c283
Tyler Schultz <tschultz@pivotal.io>
2015-04-17 17:19:34 -0700

    trying to prepare some artifactz
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- ci/concourse/jobs/prepare-artifacts.sh
+++ ci/concourse/jobs/prepare-artifacts.sh
@@ -5,7 +5,7 @@ echo "working dir is    -- $(pwd)"
 echo "working dir has   -- $(ls -la)"
 echo "bosh init version -- ${BOSH_INIT_VERSION}"
 
-cd gopath/src/github.com/cloudfoundry/bosh-init/out
+cd gopath/src/github.com/cloudfoundry/bosh-init
 echo $BOSH_INIT_VERSION > version
 mv bosh-init bosh-init-${BOSH_INIT_VERSION}-linux
 
