2c21bc7ab311864715172531a3d8eed2be20e3cc
Tyler Schultz <tschultz@pivotal.io>
2015-04-17 17:09:24 -0700

    attempt to rename executable
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

---
+++ ci/concourse/jobs/prepare-artifacts.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+BOSH_INIT_VERSION=`cat version/number`
+echo "working dir is    -- $(pwd)"
+echo "working dir has   -- $(ls -la)"
+echo "bosh init version -- ${BOSH_INIT_VERSION}"
+
+cd gopath/src/github.com/cloudfoundry/bosh-init/out
+echo $BOSH_INIT_VERSION > version
+mv bosh-init bosh-init-${BOSH_INIT_VERSION}-linux
+
+# GOARCH=amd64 GOOS=darwin
+# GOARCH=amd64 GOOS=windows
