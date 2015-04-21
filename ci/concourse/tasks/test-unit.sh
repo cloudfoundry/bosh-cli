1c108b58443d4db145b3a70d34e906f35c1b419b
Tyler Schultz <tschultz@pivotal.io>
2015-04-21 14:35:45 -0700

    rename jobs dir as tasks
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

--- ci/concourse/tasks/test-unit.sh
+++ ci/concourse/tasks/test-unit.sh
@@ -5,10 +5,6 @@ set -e -x
 export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
 export GOPATH=$(pwd)/gopath
 
-echo "go is at        -- $(which go)"
-echo "working dir is  -- $(pwd)"
-echo "working dir has -- $(ls -la)"
-
 cd gopath/src/github.com/cloudfoundry/bosh-init
 bin/clean
 bin/install-ginkgo
