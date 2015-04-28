913b1909887f251c5ea3d6703bc78adf35075f04
Corey Innis <cinnis@pivotal.io>
2015-04-27 17:30:22 -0700

    update Docker image setup to bump ruby to 2.1.6 and install with openssl

--- ci/docker/install-ruby.sh
+++ ci/docker/install-ruby.sh
@@ -3,7 +3,7 @@
 set -ex
 
 RUBY_ROOT=/usr/local/ruby
-RUBY_ARCHIVE_URL=http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.3.tar.gz
+RUBY_ARCHIVE_URL=http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.6.tar.gz
 RUBY_ARCHIVE=$(basename $RUBY_ARCHIVE_URL)
 RUBY_NAME=$(basename -s .tar.gz $RUBY_ARCHIVE_URL)
 
@@ -14,7 +14,7 @@ tar xf $RUBY_ARCHIVE
 echo "Installing ruby..."
 mkdir -p $(dirname $RUBY_ROOT)
 cd $RUBY_NAME
-./configure --prefix=$(dirname $RUBY_ROOT) --disable-install-doc
+./configure --prefix=$(dirname $RUBY_ROOT) --disable-install-doc --with-openssl-dir=/usr/include/openssl
 make
 make install
 ln -s /usr/local/$RUBY_NAME $RUBY_ROOT
