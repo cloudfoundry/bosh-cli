45d9046568df22260709fc6e4ad148efdc029bd1
Aaron Hurley <ahurley@pivotal.io>
2015-05-07 14:16:49 -0700

    Break up 100 line method into smaller methods
    [#93732082]
    
    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>

--- cmd/deployment_deleter.go
+++ cmd/deployment_deleter.go
@@ -105,26 +105,53 @@ func (c *DeploymentDeleter) DeleteDeployment(stage biui.Stage) (err error) {
 		}
 	}()
 
-	var installationManifest biinstallmanifest.Manifest
-	err = stage.PerformComplex("validating", func(stage biui.Stage) error {
-		var err error
-		var cpiReleaseRef birelmanifest.ReleaseRef
-
-		installationManifest, cpiReleaseRef, err = c.parseDeploymentManifest(stage, c.deploymentManifestPath)
-		if err != nil {
-			return err
-		}
-
-		cpiJobName := installationManifest.Template.Name
-		return c.extractAndValidateCpiRelease(stage, cpiJobName, cpiReleaseRef)
-	})
+	installationManifest, installation, err := c.installCPI(stage)
 	if err != nil {
 		return err
 	}
 
+	return installation.WithRunningRegistry(c.logger, stage, func() error {
+		return c.findAndDeleteDeployment(stage, installation, deploymentState.DirectorID, installationManifest.Mbus)
+	})
+}
+
+func (c *DeploymentDeleter) findAndDeleteDeployment(stage biui.Stage, installation biinstall.Installation, directorID, installationMbus string) error {
+	deploymentManager, err := c.deploymentManager(installation, directorID, installationMbus)
+	if err != nil {
+		return err
+	}
+
+	c.logger.Debug(c.logTag, "Finding current deployment...")
+	deployment, found, err := deploymentManager.FindCurrent()
+	if err != nil {
+		return bosherr.WrapError(err, "Finding current deployment")
+	}
+
+	err = stage.PerformComplex("deleting deployment", func(deleteStage biui.Stage) error {
+		if !found {
+			//TODO: skip? would require adding skip support to PerformComplex
+			c.logger.Debug(c.logTag, "No current deployment found...")
+			return nil
+		}
+
+		return deployment.Delete(deleteStage)
+	})
+	if err != nil {
+		return bosherr.WrapError(err, "Deleting deployment")
+	}
+
+	return deploymentManager.Cleanup(stage)
+}
+
+func (c *DeploymentDeleter) installCPI(stage biui.Stage) (biinstallmanifest.Manifest, biinstall.Installation, error) {
+	installationManifest, err := c.installationManifest(stage)
+	if err != nil {
+		return installationManifest, nil, err
+	}
+
 	installer, err := c.installerFactory.NewInstaller()
 	if err != nil {
-		return bosherr.WrapError(err, "Creating CPI Installer")
+		return installationManifest, nil, bosherr.WrapError(err, "Creating CPI Installer")
 	}
 
 	var installation biinstall.Installation
@@ -132,50 +159,41 @@ func (c *DeploymentDeleter) DeleteDeployment(stage biui.Stage) (err error) {
 		installation, err = installer.Install(installationManifest, installStage)
 		return err
 	})
+	return installationManifest, installation, err
+}
+
+func (c *DeploymentDeleter) installationManifest(stage biui.Stage) (biinstallmanifest.Manifest, error) {
+	var installationManifest biinstallmanifest.Manifest
+	err := stage.PerformComplex("validating", func(stage biui.Stage) error {
+		var err error
+		var cpiReleaseRef birelmanifest.ReleaseRef
+		installationManifest, cpiReleaseRef, err = c.parseDeploymentManifest(stage, c.deploymentManifestPath)
+		if err != nil {
+			return err
+		}
+		return c.extractAndValidateCpiRelease(stage, installationManifest.Template.Name, cpiReleaseRef)
+	})
+	return installationManifest, err
+}
+
+func (c *DeploymentDeleter) deploymentManager(installation biinstall.Installation, directorID, installationMbus string) (bidepl.Manager, error) {
+	c.logger.Debug(c.logTag, "Creating cloud client...")
+	cloud, err := c.cloudFactory.NewCloud(installation, directorID)
 	if err != nil {
-		return err
+		return nil, bosherr.WrapError(err, "Creating CPI client from CPI installation")
 	}
 
-	return installation.WithRunningRegistry(c.logger, stage, func() error {
-		c.logger.Debug(c.logTag, "Creating cloud client...")
-		cloud, err := c.cloudFactory.NewCloud(installation, deploymentState.DirectorID)
-		if err != nil {
-			return bosherr.WrapError(err, "Creating CPI client from CPI installation")
-		}
+	c.logger.Debug(c.logTag, "Creating agent client...")
+	agentClient := c.agentClientFactory.NewAgentClient(directorID, installationMbus)
 
-		c.logger.Debug(c.logTag, "Creating agent client...")
-		agentClient := c.agentClientFactory.NewAgentClient(deploymentState.DirectorID, installationManifest.Mbus)
+	c.logger.Debug(c.logTag, "Creating blobstore client...")
+	blobstore, err := c.blobstoreFactory.Create(installationMbus)
+	if err != nil {
+		return nil, bosherr.WrapError(err, "Creating blobstore client")
+	}
 
-		c.logger.Debug(c.logTag, "Creating blobstore client...")
-		blobstore, err := c.blobstoreFactory.Create(installationManifest.Mbus)
-		if err != nil {
-			return bosherr.WrapError(err, "Creating blobstore client")
-		}
-
-		c.logger.Debug(c.logTag, "Creating deployment manager...")
-		deploymentManager := c.deploymentManagerFactory.NewManager(cloud, agentClient, blobstore)
-
-		c.logger.Debug(c.logTag, "Finding current deployment...")
-		deployment, found, err := deploymentManager.FindCurrent()
-		if err != nil {
-			return bosherr.WrapError(err, "Finding current deployment")
-		}
-
-		err = stage.PerformComplex("deleting deployment", func(deleteStage biui.Stage) error {
-			if !found {
-				//TODO: skip? would require adding skip support to PerformComplex
-				c.logger.Debug(c.logTag, "No current deployment found...")
-				return nil
-			}
-
-			return deployment.Delete(deleteStage)
-		})
-		if err != nil {
-			return bosherr.WrapError(err, "Deleting deployment")
-		}
-
-		return deploymentManager.Cleanup(stage)
-	})
+	c.logger.Debug(c.logTag, "Creating deployment manager...")
+	return c.deploymentManagerFactory.NewManager(cloud, agentClient, blobstore), nil
 }
 
 func (c *DeploymentDeleter) extractAndValidateCpiRelease(stage biui.Stage, cpiJobName string, cpiReleaseRef birelmanifest.ReleaseRef) error {
