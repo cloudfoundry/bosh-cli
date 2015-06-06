c98dacb129d15b03a20b1ff76e6c94da935dec7f
Ted Young <tyoung@pivotal.io>
2015-06-05 18:16:27 -0700

    deleting leaked acceptance credentials
    Signed-off-by: Tim Hausler <thausler@pivotal.io>

--- env.sh
+++
@@ -1,6 +0,0 @@
-export BOSH_AWS_ACCESS_KEY_ID=AKIAJNZAY225YQ2DXYEQ
-export BOSH_AWS_SECRET_ACCESS_KEY=hWKPpXW6A9nRl\+6bkXbw\+RQ61RAbOO/dS9dNXIVz
-export BOSH_LITE_KEYPAIR="bosh-dev"
-export BOSH_LITE_SUBNET_ID="subnet-b1ea9a99"
-export BOSH_LITE_SECURITY_GROUP="sg-9da648f8"
-export BOSH_LITE_PRIVATE_KEY="/Users/pivotal/workspace/deployments-bosh/keys/bosh-dev.key"
