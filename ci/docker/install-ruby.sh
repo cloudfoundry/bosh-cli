4e43ff33aea8044cf5189a56e43d964c6e81c933
Dennis Leon <dleon@pivotal.io>
2016-10-05 09:36:36 -0700

    Upgrade Ruby and Vagrant AWS Plugin in Docker image
    [#131001637]
    
    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>

--- ci/docker/install-ruby.sh
+++ ci/docker/install-ruby.sh
@@ -3,12 +3,14 @@
 set -ex
 
 RUBY_ROOT=/usr/local/ruby
-RUBY_ARCHIVE_URL=http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.6.tar.gz
+RUBY_ARCHIVE_URL=http://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.1.tar.gz
 RUBY_ARCHIVE=$(basename $RUBY_ARCHIVE_URL)
 RUBY_NAME=$(basename -s .tar.gz $RUBY_ARCHIVE_URL)
-
+RUBY_DOWNLOAD_SHA256=b87c738cb2032bf4920fef8e3864dc5cf8eae9d89d8d523ce0236945c5797dcd
 echo "Downloading ruby..."
-wget -q $RUBY_ARCHIVE_URL
+curl -fSL -o $RUBY_ARCHIVE $RUBY_ARCHIVE_URL
+echo "$RUBY_DOWNLOAD_SHA256 $RUBY_ARCHIVE" | sha256sum -c -
+
 tar xf $RUBY_ARCHIVE
 
 echo "Installing ruby..."
