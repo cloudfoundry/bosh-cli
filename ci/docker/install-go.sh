424b51ef65e1d1a31d86fc657da6c0f2c6c6026a
Tyler Schultz <tschultz@pivotal.io>
2016-10-05 09:37:35 -0700

    Upgrade go to 1.7.1 in the Docker image
    [#131001637]
    
    Signed-off-by: Dennis Leon <dleon@pivotal.io>

--- ci/docker/install-go.sh
+++ ci/docker/install-go.sh
@@ -3,8 +3,8 @@
 set -ex
 
 GOROOT=/usr/local/go
-GO_ARCHIVE_URL=https://storage.googleapis.com/golang/go1.6.1.linux-amd64.tar.gz
-GO_ARCHIVE_SHA256=6d894da8b4ad3f7f6c295db0d73ccc3646bce630e1c43e662a0120681d47e988
+GO_ARCHIVE_URL=https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz
+GO_ARCHIVE_SHA256=43ad621c9b014cde8db17393dc108378d37bc853aa351a6c74bf6432c1bbd182
 GO_ARCHIVE=$(basename $GO_ARCHIVE_URL)
 
 echo "Downloading go..."
@@ -13,12 +13,3 @@ wget -q $GO_ARCHIVE_URL
 echo "${GO_ARCHIVE_SHA256} ${GO_ARCHIVE}" | sha256sum -c -
 tar xf $GO_ARCHIVE -C $(dirname $GOROOT)
 chmod -R a+w $GOROOT
-
-(
-  cd $GOROOT/src
-  sudo GOOS=darwin GOARCH=amd64 ./make.bash --no-clean
-)
-
-export GOROOT
-export GOPATH=$GOROOT
-export PATH=$GOROOT/bin:$PATH
