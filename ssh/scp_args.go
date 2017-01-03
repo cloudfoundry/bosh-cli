6c87de0f73aaaca7c20b2d408fa53f3cbf433cb1
dmitriy kalinin <dmitriy@pivotallabs.com>
2017-01-02 17:53:53 -0800

    add %i so that bosh scp can download files into instance namespaced directories

--- ssh/scp_args.go
+++ ssh/scp_args.go
@@ -47,6 +47,11 @@ func (a SCPArgs) ForHost(host boshdir.Host) []string {
 			pieces[0] = fmt.Sprintf("%s@%s", host.Username, host.Host)
 		}
 
+		for i, _ := range pieces {
+			// Replace special identifier '%i' with specific instance
+			pieces[i] = strings.Replace(pieces[i], "%i", host.IndexOrID, -1)
+		}
+
 		args = append(args, strings.Join(pieces, ":"))
 	}
 
