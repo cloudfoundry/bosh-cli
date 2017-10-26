9dafc8c834d1d433256ceb849de9ba728779a161
Genevieve LEsperance <glesperance@pivotal.io>
2017-10-26 12:18:00 -0700

    Start socks5 proxy for every ssh session
    - when BOSH_ALL_PROXY has sssh+socks5:// schema.
    
    [#152085398]
    
    Signed-off-by: Rowan Jacobs <rojacobs@pivotal.io>

--- vendor/github.com/vito/go-interact/interact/userio_test.go
+++ vendor/github.com/vito/go-interact/interact/userio_test.go
@@ -1,6 +1,9 @@
 package interact_test
 
 import (
+	"io/ioutil"
+	"os"
+
 	"github.com/kr/pty"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -30,6 +33,39 @@ var _ = Describe("User IO", func() {
 				err = interaction.Resolve(&thing)
 
 				Expect(err).To(Equal(interact.ErrKeyboardInterrupt))
+			})
+		})
+	})
+
+	Describe("fetching input from a non-TTY user", func() {
+		Context("when passed a CRLF", func() {
+			var input, output *os.File
+
+			BeforeEach(func() {
+				var err error
+				input, err = ioutil.TempFile("", "go-interact-input")
+				Expect(err).ToNot(HaveOccurred())
+				err = ioutil.WriteFile(input.Name(), []byte("What do you mean? An African or European swallow?\r\n"), 0644)
+				Expect(err).ToNot(HaveOccurred())
+
+				output, err = ioutil.TempFile("", "go-interact-output")
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			AfterEach(func() {
+				Expect(os.RemoveAll(input.Name())).ToNot(HaveOccurred())
+				Expect(os.RemoveAll(output.Name())).ToNot(HaveOccurred())
+			})
+
+			It("ignores the CR", func() {
+				interaction := interact.NewInteraction("What is the air-speed of a Swallow?")
+				interaction.Input = input
+				interaction.Output = output
+
+				var thing string
+				err := interaction.Resolve(&thing)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(thing).To(Equal("What do you mean? An African or European swallow?"))
 			})
 		})
 	})
