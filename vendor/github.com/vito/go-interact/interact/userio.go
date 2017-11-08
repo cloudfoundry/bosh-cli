4b1ad51ab3121c923715c00e4a0caac7f89172a2
Difan Zhao <dzhao@pivotal.io>
2017-11-08 11:30:29 -0800

    Downgrade go-interact to get around bad ioctl error
    Signed-off-by: Luan Santos <lsantos@pivotal.io>

--- vendor/github.com/vito/go-interact/interact/userio.go
+++ vendor/github.com/vito/go-interact/interact/userio.go
@@ -22,10 +22,10 @@ type ttyUser struct {
 
 var ErrKeyboardInterrupt = errors.New("keyboard interrupt")
 
-func newTTYUser(input io.Reader, output *os.File) (ttyUser, error) {
+func newTTYUser(input *os.File, output io.Writer) (ttyUser, error) {
 	term := terminal.NewTerminal(readWriter{input, output}, "")
 
-	width, height, err := terminal.GetSize(int(output.Fd()))
+	width, height, err := terminal.GetSize(int(input.Fd()))
 	if err != nil {
 		return ttyUser{}, err
 	}
@@ -120,8 +120,6 @@ func (u nonTTYUser) readLine() (string, error) {
 		if n == 1 {
 			if chr[0] == '\n' {
 				return line, nil
-			} else if chr[0] == '\r' {
-				continue
 			}
 
 			line += string(chr)
