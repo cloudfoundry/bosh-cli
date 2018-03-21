f2389f8423035c99988e2c5f9d5b213fa1e20eb6
Mauro Morales <mamorales@suse.com>
2018-03-21 15:26:15 +0100

    Revert "Allow diff-config to work with local files"
    This reverts commit dc6ad46b54c01aa25a6d08995c5229d31414dba3.

--- cmd/diff_config.go
+++ cmd/diff_config.go
@@ -15,14 +15,17 @@ func NewDiffConfigCmd(ui boshui.UI, director boshdir.Director) DiffConfigCmd {
 }
 
 func (c DiffConfigCmd) Run(opts DiffConfigOpts) error {
-	configDiff, err := c.director.DiffConfigByIDOrContent(opts.FromID, opts.FromContent.Bytes, opts.ToID, opts.ToContent.Bytes)
+
+	fromID := opts.Args.FromID
+	toID := opts.Args.ToID
+
+	configDiff, err := c.director.DiffConfigByID(fromID, toID)
 	if err != nil {
 		return err
 	}
 
 	diff := NewDiff(configDiff.Diff)
-
-	ConfigDiffTable{diff, opts, c.ui}.Print()
+	diff.Print(c.ui)
 
 	return nil
 }
--- cmd/diff_config_test.go
+++ cmd/diff_config_test.go
@@ -10,7 +10,6 @@ import (
 	boshdir "github.com/cloudfoundry/bosh-cli/director"
 	fakedir "github.com/cloudfoundry/bosh-cli/director/directorfakes"
 	fakeui "github.com/cloudfoundry/bosh-cli/ui/fakes"
-	boshtbl "github.com/cloudfoundry/bosh-cli/ui/table"
 )
 
 var _ = Describe("DiffConfigCmd", func() {
@@ -33,8 +32,10 @@ var _ = Describe("DiffConfigCmd", func() {
 
 		BeforeEach(func() {
 			opts = DiffConfigOpts{
-				FromID: "1",
-				ToID:   "2",
+				Args: DiffConfigArgs{
+					FromID: "1",
+					ToID:   "2",
+				},
 			}
 		})
 
@@ -44,15 +45,15 @@ var _ = Describe("DiffConfigCmd", func() {
 			err := act()
 			Expect(err).ToNot(HaveOccurred())
 
-			Expect(director.DiffConfigByIDOrContentCallCount()).To(Equal(1))
+			Expect(director.DiffConfigByIDCallCount()).To(Equal(1))
 
-			from, _, to, _ := director.DiffConfigByIDOrContentArgsForCall(0)
+			from, to := director.DiffConfigByIDArgsForCall(0)
 			Expect(from).To(Equal("1"))
 			Expect(to).To(Equal("2"))
 		})
 
 		It("returns an error if diffing failed", func() {
-			director.DiffConfigByIDOrContentReturns(boshdir.ConfigDiff{}, errors.New("Fetching diff result"))
+			director.DiffConfigByIDReturns(boshdir.ConfigDiff{}, errors.New("Fetching diff result"))
 
 			err := act()
 			Expect(err).To(HaveOccurred())
@@ -60,40 +61,19 @@ var _ = Describe("DiffConfigCmd", func() {
 
 		It("gets the diff from the two configs", func() {
 			diff := [][]interface{}{
-				{"some line that stayed", nil},
-				{"some line that was added", "added"},
-				{"some line that was removed", "removed"},
+				[]interface{}{"some line that stayed", nil},
+				[]interface{}{"some line that was added", "added"},
+				[]interface{}{"some line that was removed", "removed"},
 			}
 
 			expectedDiff := boshdir.NewConfigDiff(diff)
-			director.DiffConfigByIDOrContentReturns(expectedDiff, nil)
+			director.DiffConfigByIDReturns(expectedDiff, nil)
 			err := act()
 			Expect(err).ToNot(HaveOccurred())
-			Expect(ui.Table).To(Equal(
-				boshtbl.Table{
-					Content: "",
-
-					Header: []boshtbl.Header{
-						boshtbl.NewHeader("From ID"),
-						boshtbl.NewHeader("To ID"),
-						boshtbl.NewHeader("Diff"),
-					},
-
-					Rows: [][]boshtbl.Value{
-						{
-							boshtbl.NewValueString("1"),
-							boshtbl.NewValueString("2"),
-							boshtbl.NewValueString("  some line that stayed\n+ some line that was added\n- some line that was removed\n"),
-						},
-					},
-
-					Notes: []string{},
-
-					FillFirstColumn: true,
-
-					Transpose: true,
-				}))
-			Expect(director.DiffConfigByIDOrContentCallCount()).To(Equal(1))
+			Expect(director.DiffConfigByIDCallCount()).To(Equal(1))
+			Expect(ui.Said).To(ContainElement("  some line that stayed\n"))
+			Expect(ui.Said).To(ContainElement("+ some line that was added\n"))
+			Expect(ui.Said).To(ContainElement("- some line that was removed\n"))
 		})
 	})
 })
