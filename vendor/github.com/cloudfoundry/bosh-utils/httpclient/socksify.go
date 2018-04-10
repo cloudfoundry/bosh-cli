c93f08c0794e52e7be0aa16eddbf0a95e95c536c
Joshua Aresty <joshua.aresty@emc.com>
2018-04-10 14:18:40 -0700

    Bump bosh-utils
    [#156638351](https://www.pivotaltracker.com/story/show/156638351)
    
    Signed-off-by: Josh Russett <jrussett@pivotal.io>

--- vendor/github.com/cloudfoundry/bosh-utils/httpclient/socksify.go
+++ vendor/github.com/cloudfoundry/bosh-utils/httpclient/socksify.go
@@ -41,6 +41,11 @@ func SOCKS5DialFuncFromEnvironment(origDialer DialFunc, socks5Proxy ProxyDialer)
 			return origDialer
 		}
 
+		username := ""
+		if proxyURL.User != nil {
+			username = proxyURL.User.Username()
+		}
+
 		proxySSHKeyPath, ok := queryMap["private-key"]
 		if !ok {
 			return origDialer
@@ -71,7 +76,7 @@ func SOCKS5DialFuncFromEnvironment(origDialer DialFunc, socks5Proxy ProxyDialer)
 			mut.Lock()
 			defer mut.Unlock()
 			if dialer == nil {
-				proxyDialer, err := socks5Proxy.Dialer("", string(proxySSHKey), proxyURL.Host)
+				proxyDialer, err := socks5Proxy.Dialer(username, string(proxySSHKey), proxyURL.Host)
 				if err != nil {
 					return nil, bosherr.WrapErrorf(err, "Creating SOCKS5 dialer")
 				}
