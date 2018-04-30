69ae012526816c818c610155f3ac277b431325d6
Luan Santos <lsantos@pivotal.io>
2018-04-30 10:53:24 -0700

    Bump go to 1.9.2
    [#157146768](https://www.pivotaltracker.com/story/show/157146768)
    
    Co-authored-by: Joshua Aresty <joshua.aresty@emc.com>

--- ci/tasks/test-unit.sh
+++ ci/tasks/test-unit.sh
@@ -8,8 +8,6 @@ export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
 export GOPATH=$(pwd)/gopath
 
 cd gopath/src/github.com/cloudfoundry/bosh-cli
-source ci/docker/deps-golang-1.7.1
-bin/require-ci-golang-version
 bin/clean
 bin/install-ginkgo
 bin/test-prepare
