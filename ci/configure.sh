e2181e891dfcacb0f1450d23288c63628e87e37b
Charles Hansen <chansen@pivotal.io>
2019-06-06 15:09:56 -0700

    Move bosh-cli pipeline to bosh-director team
    [#166391315](https://www.pivotaltracker.com/story/show/166391315)

--- ci/configure.sh
+++ ci/configure.sh
@@ -4,5 +4,5 @@ set -e
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-exec fly -t "${CONCOURSE_TARGET:-production}" set-pipeline -p bosh:cli -c ./pipeline.yml \
+exec fly -t "${CONCOURSE_TARGET:-director}" set-pipeline -p bosh:cli -c ./pipeline.yml \
   --load-vars-from <(lpass show -G "bosh-cli concourse secrets" --notes)
