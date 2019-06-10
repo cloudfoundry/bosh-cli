5e8e5e72cd998c632361afffead8769d847bc3b3
James Myers <jmyers@pivotal.io>
2019-06-10 14:30:26 -0700

    Move bosh-cli docker images pipeline to the director team
    [#166391315](https://www.pivotaltracker.com/story/show/166391315)
    
    Co-authored-by: Belinda Liu <bliu@pivotal.io>

--- ci/docker/configure.sh
+++ ci/docker/configure.sh
@@ -4,5 +4,5 @@ set -e
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-exec fly -t "${CONCOURSE_TARGET:-production}" set-pipeline -p bosh-cli:docker-images -c ./pipeline.yml \
+exec fly -t "${CONCOURSE_TARGET:-director}" set-pipeline -p bosh-cli:docker-images -c ./pipeline.yml \
   --load-vars-from <(lpass show -G "bosh-cli:docker-images concourse secrets" --notes)
