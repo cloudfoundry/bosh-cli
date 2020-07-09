3ad54853b1bd08ec2840657d9313052a357b1b2d
Kevin Rutten <krutten@starkandwayne.com>
2020-07-08 22:13:33 -0700

    Update Curl test for bosh-util bump with optional keepalive
    [#173690103](https://www.pivotaltracker.com/story/show/173690103)

--- cmd/curl_test.go
+++ cmd/curl_test.go
@@ -29,7 +29,7 @@ var _ = Describe("CurlCmd", func() {
 		logger := boshlog.NewLogger(boshlog.LevelNone)
 		command = NewCurlCmd(ui, boshdir.NewClientRequest(
 			server.URL(),
-			boshhttp.NewHTTPClient(boshhttp.CreateDefaultClient(nil), logger),
+			boshhttp.NewHTTPClient(boshhttp.CreateKeepAliveDefaultClient(nil), logger),
 			boshdir.NewNoopFileReporter(),
 			logger,
 		))
