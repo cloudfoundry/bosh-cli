9895ea1cff5e0048a6904699e37bc080118411e4
aram price <pricear@vmware.com>
2022-04-19 11:41:07 -0700

    Introdoce `golangci-lint` and `goimports`
    - Add convienence scripts `bin/format` and `bin/lint`
    - Update go.mod and vendor/

--- tools/tools.go
+++ tools/tools.go
@@ -4,9 +4,11 @@
 package tools
 
 import (
-	_ "github.com/golang/mock/mockgen/model"     // comment to make golint happy
-	_ "github.com/maxbrunsfeld/counterfeiter/v6" // comment to make golint happy
-	_ "github.com/onsi/ginkgo/ginkgo"            // comment to make golint happy
+	_ "github.com/golang/mock/mockgen/model"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/maxbrunsfeld/counterfeiter/v6"
+	_ "github.com/onsi/ginkgo/ginkgo"
+	_ "golang.org/x/tools/cmd/goimports"
 )
 
 // This file imports packages that are used when running go generate, or used
