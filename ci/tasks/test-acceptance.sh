50370807e424c11c2b167ec7e9a8d8b5dec0b74e
aram price <pricear@vmware.com>
2022-04-20 15:13:58 -0700

    Remove dead code in ci/tasks/test-acceptance.sh

--- ci/tasks/test-acceptance.sh
+++ ci/tasks/test-acceptance.sh
@@ -3,16 +3,6 @@ set -eu -o pipefail
 
 set -x
 
-ensure_not_replace_value() { # TODO this does not appear to be used
-  local name=$1
-  local value
-  value=$(eval echo "\$${name}")
-  if [ "$value" == 'replace-me' ]; then
-    echo "environment variable $name must be set"
-    exit 1
-  fi
-}
-
 if [[ $(whoami) != "root" ]]; then
   echo "acceptance tests must be run as a privileged user"
   exit 1
@@ -25,6 +15,6 @@ export BOSH_INIT_STEMCELL_PATH
 
 cd bosh-cli
 
-start-garden 1> /dev/null
+start-garden 1>/dev/null
 
-base=$PWD ./bin/test-acceptance-with-garden
+bin/test-acceptance-with-garden
