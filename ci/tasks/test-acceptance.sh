235a3d8bdef18fa39b58aa1c86f60437c980a126
aram price <pricear@vmware.com>
2022-04-20 14:49:26 -0700

    Fix shellcheck issues: ci/tasks/test-acceptance.sh

--- ci/tasks/test-acceptance.sh
+++ ci/tasks/test-acceptance.sh
@@ -3,9 +3,10 @@ set -eu -o pipefail
 
 set -x
 
-ensure_not_replace_value() {
+ensure_not_replace_value() { # TODO this does not appear to be used
   local name=$1
-  local value=$(eval echo '$'$name)
+  local value
+  value=$(eval echo "\$${name}")
   if [ "$value" == 'replace-me' ]; then
     echo "environment variable $name must be set"
     exit 1
@@ -13,14 +14,17 @@ ensure_not_replace_value() {
 }
 
 set +x
-if [[ `whoami` != "root" ]]; then
+if [[ $(whoami) != "root" ]]; then
   echo "acceptance tests must be run as a privileged user"
   exit 1
 fi
 set -x
 
-export BOSH_INIT_CPI_RELEASE_PATH=$(ls $PWD/cpi-release/*.tgz)
-export BOSH_INIT_STEMCELL_PATH=$(ls $PWD/stemcell/*.tgz)
+BOSH_INIT_CPI_RELEASE_PATH=$(ls "${PWD}/cpi-release/*.tgz")
+BOSH_INIT_STEMCELL_PATH=$(ls "${PWD}/stemcell/*.tgz")
+
+export BOSH_INIT_CPI_RELEASE_PATH
+export BOSH_INIT_STEMCELL_PATH
 
 cd bosh-cli
 
