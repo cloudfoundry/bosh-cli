3bebfe94c44db99ad535b3b2f8d68147dfed79c4
Joseph Palermo <jpalermo@pivotal.io>
2023-01-11 14:05:13 -0800

    Update ci pipeline with new credhub entries

--- ci/configure.sh
+++ ci/configure.sh
@@ -3,5 +3,4 @@ set -eu -o pipefail
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-exec fly -t bosh-ecosystem set-pipeline -p bosh-cli -c ./pipeline.yml \
-  --load-vars-from <(lpass show -G "bosh-cli concourse secrets" --notes)
+exec fly -t bosh-ecosystem set-pipeline -p bosh-cli -c ./pipeline.yml
