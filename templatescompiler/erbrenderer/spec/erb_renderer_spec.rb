868c6986ec370f93e329fb8867e78dbe15872d22
aram price <aram.price@broadcom.com>
2025-12-05 16:20:19 -0800

    Add minimal spec for ERBRenderer#render

--- templatescompiler/erbrenderer/spec/erb_renderer_spec.rb
+++ templatescompiler/erbrenderer/spec/erb_renderer_spec.rb
@@ -3,15 +3,72 @@ require "erb_renderer"
 
 RSpec.describe "erb_renderer" do
   describe "ERBRenderer" do
-    let(:json_context_path) { File.join(Dir.tmpdir, "context.json") }
+    let(:test_tmpdir) { Dir.tmpdir }
+    let(:json_context_path) { File.join(test_tmpdir, "context.json") }
+
+    before do
+      File.write(json_context_path, context_hash.to_json)
+    end
 
     describe "#initialize" do
-      before do
-        File.write(json_context_path, {}.to_json)
-      end
+      let(:context_hash) { {} }
 
       it "does not raise an error" do
         expect { ERBRenderer.new(json_context_path) }.not_to raise_error
+      end
+    end
+
+    describe "#render" do
+      let(:context_hash) do
+        {
+          index: 867_5309,
+          global_properties: {
+            property1: "global_value1"
+          },
+          cluster_properties: {
+            property2: "cluster_value1"
+          },
+          default_properties: {
+            property1: "default_value1",
+            property2: "default_value2",
+            property3: "default_value3"
+          }
+        }
+      end
+
+      let(:erb_template_path) { File.join(test_tmpdir, "template.yml.erb") }
+      let(:erb_content) do
+        <<~TEST_TEMPLATE
+          ---
+          property1: <%= p('property1') %>
+          property2: <%= p('property2') %>
+          property3: <%= p('property3') %>
+        TEST_TEMPLATE
+      end
+
+      let(:rendered_template_path) { File.join(test_tmpdir, "template.yml") }
+      let(:expected_template_content) do
+        <<~EXPECTED_TEMPLATE
+          ---
+          property1: global_value1
+          property2: cluster_value1
+          property3: default_value3
+        EXPECTED_TEMPLATE
+      end
+
+      let(:erb_renderer) { ERBRenderer.new(json_context_path) }
+
+      before do
+        File.write(erb_template_path, erb_content)
+      end
+
+      it "does not raise an error" do
+        expect { erb_renderer.render(erb_template_path, rendered_template_path) }.not_to raise_error
+      end
+
+      it "renders the expected content" do
+        erb_renderer.render(erb_template_path, rendered_template_path)
+        expect(File.read(rendered_template_path)).to eq(expected_template_content)
       end
     end
   end
